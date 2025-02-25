daed5cc6b0424e597f7df5c89482eb995a99ba6e
ATOROYO <davidatoroyosika@gmail.com>
2025-02-25 22:11:56 +0300

    Deleted a code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import "./App.css";
-
-export default function App() {}
+export default function App() {
+  return (
+    <div>
+      <h1>Hellom there</h1>
+      <h2>Start coding</h2>
+    </div>
+  );
+}
 
 function Counter() {}
